03f766a370e13dae1ebd761e7a49c1ca927bb768
jdcauley <jordan@cauley.co>
2015-01-14 23:01:45 -0500

    idk

--- api/services/utility.js
+++ api/services/utility.js
@@ -9,50 +9,56 @@ module.exports = {
       var urlTitle = episode.title
       var urlTitle = urlTitle.replace(/ /g,"-");
 
-        Episodes.findOneByPrimaryEnclosureUrl(episode.enclosures[0].url, function(err, found){
-          if(err){
-            callback();
-          }
-          if(found){
-            callback();
-          } else {
-            var theEnclosure = episode.enclosures[0].url;
-            var fileType = mime.lookup(theEnclosure);
-            var minusExt = theEnclosure.substr(0, theEnclosure.lastIndexOf('.')) || theEnclosure;
+        if(episode.enclosures[0].url){
 
-            if(episode['content:encoded']){
-              var content = episode['content:encoded']['#'];
-            } else {
-              var content = null;
+          Episodes.findOneByPrimaryEnclosureUrl(episode.enclosures[0].url, function(err, found){
+            if(err){
+              callback();
             }
-            Episodes.create({
-              feed: feedId,
-              title: episode.title,
-              urlTitle: urlTitle,
-              extFree: minusExt,
-              fileType: fileType,
-              description: episode.description,
-              summary: episode.summary,
-              pubDate: episode.pubDate,
-              episodePage: episode.link,
-              publisher: episode.author,
-              image: episode.image.url,
-              primaryEnclosureUrl: episode.enclosures[0].url,
-              allEnclosures: episode.enclosures,
-              content: content
-            }, function(err, newEpisode){
-              if(err){
-                callback();
+            if(found){
+              callback();
+            } else {
+              var theEnclosure = episode.enclosures[0].url;
+              var fileType = mime.lookup(theEnclosure);
+              var minusExt = theEnclosure.substr(0, theEnclosure.lastIndexOf('.')) || theEnclosure;
+
+              if(episode['content:encoded']){
+                var content = episode['content:encoded']['#'];
+              } else {
+                var content = null;
               }
-              if(newEpisode){
-                callback();
-              }
+              Episodes.create({
+                feed: feedId,
+                title: episode.title,
+                urlTitle: urlTitle,
+                extFree: minusExt,
+                fileType: fileType,
+                description: episode.description,
+                summary: episode.summary,
+                pubDate: episode.pubDate,
+                episodePage: episode.link,
+                publisher: episode.author,
+                image: episode.image.url,
+                primaryEnclosureUrl: episode.enclosures[0].url,
+                allEnclosures: episode.enclosures,
+                content: content
+              }, function(err, newEpisode){
+                if(err){
+                  callback();
+                }
+                if(newEpisode){
+                  callback();
+                }
 
 
 
-            });
-          }
-        });
+              });
+            }
+          });
+
+        } else {
+          callback();
+        }
 
     }, function(err){
       if(err) cb(err);
